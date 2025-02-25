8d9a48145f9cbb6d40ab848e4f8075133828e09c
c-jianyu <jianyu329@gmail.com>
2025-02-25 16:29:10 +0800

    02250429

--- FCPS_eUnits/Unit1/src/Lab00.java
+++ FCPS_eUnits/Unit1/src/Lab00.java
@@ -4,15 +4,25 @@ import edu.fcps.karel2.Robot;
 
 public class Lab00 {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		Display.openWorld("maps/first.map");
+//565
+		Display.openWorld("../maps/first.map");
+      Display.setSize(10,10);
+      Display.setSpeed(5);
+      
 		Robot karel = new Robot();
       
+      karel.move();
+      karel.pickBeeper();
+      karel.move();
+      karel.turnLeft();
+      karel.move();
+      karel.putBeeper();
+      karel.move();
+      karel.turnLeft();
+      karel.turnLeft();
+   
 	}
 
 }
