7cbb29e8d1274496a9bcba711414229d2ad0516b
c-jianyu <jianyu329@gmail.com>
2025-03-10 16:22:40 +0800

    Update SteepleChaseRacer_ans.java

--- FCPS_eUnits/Unit1/src/SteepleChaseRacer_ans.java
+++ FCPS_eUnits/Unit1/src/SteepleChaseRacer_ans.java
@@ -1,48 +1,27 @@
 
-public class SteepleChaseRacer_ans extends Racer
+public class BoxTopRacer_ans extends Racer
 {
-    public SteepleChaseRacer()
+    public BoxTopRacer()
     {
     }
-    public SteepleChaseRacer(int y)
+    public BoxTopRacer(int y)
     {
         super(y);
     }
 
     public void jumpRight()
     {
-         turnLeft();
-         move();
-         turnRight();
-         move();
-         turnRight();
-         move();
-         turnLeft();
-    }
-    public void jumpLeft()
-    {
-         turnRight();
-         move();
-         turnLeft();
-         move();
-         turnLeft();
-         move();
-         turnRight();
-    }
-
-    public void sprint(int n)
-    {
-         for(int k=1; k <= n; k++) move();
-    }
-    
-    public void put(int n)
-    {
-         for(int k=1; k <= n; k++) putBeeper();
-    }
-    
-    public void pick(int n)
-    {
-         for(int k=1; k <= n; k++) pickBeeper();
+        turnLeft();
+        while(!rightIsClear()){
+            move();
+        }
+        turnRight();
+        move();
+        turnRight();
+        while(frontIsClear()){
+            move();
+        }
+        turnLeft(); 
     }
 
 }
