dcbc4ac8c9741954b7dfa7ddc0ba2afb4b0acd14
c-jianyu <jianyu329@gmail.com>
2025-03-10 21:15:17 +0800

    03100915

--- FCPS_eUnits/Unit1/src/Lab00.java
+++ FCPS_eUnits/Unit1/src/Lab00.java
@@ -7,7 +7,7 @@ public class Lab00 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Display.openWorld("../maps/first.map");
+		Display.openWorld("C:\\Users\\user\\Documents\\GitHub\\Java_Course_v1122\\FCPS_eUnits\\Unit1\\maps\\first.map");
       Display.setSize(10,10);
       Display.setSpeed(10);
       
