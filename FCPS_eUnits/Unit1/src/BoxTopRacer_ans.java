e9133bcefca97eef4e255bde5a5e67c7a91a590f
c-jianyu <jianyu329@gmail.com>
2025-03-10 16:19:28 +0800

    Create BoxTopRacer_ans.java

---
+++ FCPS_eUnits/Unit1/src/BoxTopRacer_ans.java
@@ -0,0 +1,27 @@
+
+public class BoxTopRacer_ans extends Racer
+{
+    public BoxTopRacer()
+    {
+    }
+    public BoxTopRacer(int y)
+    {
+        super(y);
+    }
+
+    public void jumpRight()
+    {
+         turnLeft();
+        while(!rightIsClear()){
+            move();
+        }
+        turnRight();
+        move();
+        turnRight();
+        while(frontIsClear()){
+            move();
+        }
+        turnLeft(); 
+    }
+
+}
