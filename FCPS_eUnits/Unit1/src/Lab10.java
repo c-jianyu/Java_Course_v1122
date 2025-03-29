046483fe58c8e11b297c6c6e6c0919f8969f24f4
c-jianyu <jianyu329@gmail.com>
2025-03-29 11:26:59 +0800

    new

--- FCPS_eUnits/Unit1/src/Lab10.java
+++ FCPS_eUnits/Unit1/src/Lab10.java
@@ -3,7 +3,7 @@ import edu.fcps.karel2.Display;
 public class Lab10 {
 
 	public static void main(String[] args) {
-		Display.openWorld("C:\\Users\\user\\Documents\\GitHub\\Java_Course_v1122\\FCPS_eUnits\\Unit1\\maps\\maze3.map");
+		Display.openWorld("C:\\Users\\user\\Documents\\GitHub\\Java_Course_v1122\\FCPS_eUnits\\Unit1\\maps\\maze1.map");
         Display.setSize(10, 10);
         Display.setSpeed(10);
         mazePlayer pete = new mazePlayer(1,1,Display.NORTH,Display.INFINITY);
