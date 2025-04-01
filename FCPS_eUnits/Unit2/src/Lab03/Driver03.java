683b553c2576c0892a605e31027a4f8b075d129d
c-jianyu <jianyu329@gmail.com>
2025-04-01 17:27:46 +0800

    04010527good

---
+++ FCPS_eUnits/Unit2/src/Lab03/Driver03.java
@@ -0,0 +1,18 @@
+package Lab03;
+
+import javax.swing.JFrame;
+
+import Lab01.Panel01;
+
+public class Driver03 {
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Lab03");
+		frame.setSize(400, 400);
+		frame.setLocation(100, 50);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setContentPane(new Panel03());
+		frame.setVisible(true);
+	}
+
+}
